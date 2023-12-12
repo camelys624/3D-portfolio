9112dbaaa9ba13136a35a709a6e93ca2c64778b7
Robbie <robbie.yang@hp.com>
2023-12-12 17:24:26 +0800

    feat: contact页面完成

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,11 @@
-import { useRef, useState } from "react"
+/* eslint-disable react/no-unknown-property */
+import { Suspense, useRef, useState } from "react"
 import emailjs from "@emailjs/browser"
+import { Canvas } from "@react-three/fiber"
+import Fox from "../models/Fox"
+import Loader from "../components/Loader"
+import useAlert from "../hooks/useAlert"
+import Alert from "../components/Alert"
 
 const Contact = () => {
   const formRef = useRef(null)
@@ -9,32 +15,60 @@ const Contact = () => {
     message: "",
   })
   const [loading, setLoading] = useState(false)
+  const [currentAnimation, setCurrentAnimation] = useState("idle")
 
-  const handleChange = (e) => {
+  const {alert, showAlert, hideAlert} = useAlert()
+
+  const handleChange = (e) =>  {
     setForm({ ...form, [e.target.name]: e.target.value })
   }
 
-  const handleFocus = () => {}
-  const handleBlur = () => {}
+  const handleFocus = () => setCurrentAnimation('walk')
+  const handleBlur = () => setCurrentAnimation('idle')
   const handleSubmit = (e) => {
     e.preventDefault()
     setLoading(true)
+    setCurrentAnimation('hit')
 
-    emailjs.sendForm(
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: form.name,
-        to_name: '周文峰',
-        fromm_email: form.email,
-        to_email: '',
-        message: form.message
-      }
-    )
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "周文峰",
+          from_email: form.email,
+          to_email: "",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setLoading(false)
+
+        setTimeout(() => {
+          hideAlert()
+          setCurrentAnimation('idle')
+        }, 3000)
+        
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        })
+        showAlert({type: "success", text: "发送成功！"})
+      })
+      .catch((error) => {
+        setLoading(false)
+        setCurrentAnimation('idle')
+        console.log(error)
+        showAlert({type: "danger", text: "发送失败！"})
+      })
   }
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert}/>}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">联系方式</h1>
 
@@ -79,7 +113,7 @@ const Contact = () => {
               name="message"
               className="textarea"
               placeholder="我可以帮你什么？"
-              value={form.name}
+              value={form.message}
               onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
@@ -95,6 +129,20 @@ const Contact = () => {
             {loading ? "发送中..." : "发送"}
           </button>
         </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md-h-[550px] h-[350px]">
+        <Canvas camera={{ position: [0, 0, 5], fov: 75, near: 0.1, far: 1000 }}>
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={null}>
+            <Fox
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+              currentAnimation={currentAnimation}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
