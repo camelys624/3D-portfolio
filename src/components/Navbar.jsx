fb92bf56cfa7f6eafca60614445994046bd0f0d1
Robbie <robbie.yang@hp.com>
2023-12-14 17:28:40 +0800

    feat: about页面完成

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,6 @@ const Navbar = () => {
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>About</NavLink>
         <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>Projects</NavLink>
-        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>Proj </NavLink>
       </nav>
     </header>
   )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,35 @@
-
+import { projects } from "../constants"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="max-container">
+      <h1 className="head-text">
+        我的
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          项目们
+        </span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>在校期间鼓捣了一些小项目</p>
+      </div>
+
+      <div className="flex flex-wrap my-30 gap-16">
+        {projects.map((project) => (
+          <div
+            key={project.name}
+            className="lg:w-[400px] w-full"
+          >
+            <div>
+              <div className={`btn-back rounded-xl ${project.theme}`}></div>
+              <div>
+                <img src={project.iconUrl} alt="Project Icon" className="w-1/2 h-1/2 object-contain" />
+              </div>
+            </div>
+            </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
